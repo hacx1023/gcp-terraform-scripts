39e4d95b73584e4fcc7bde8c0d713803bde1a9b8
Shubham Kumar <69086696+hacx1023@users.noreply.github.com>
2025-11-08 22:52:48 +0530

    Update main.tf
    Terraform file is updated

--- main.tf
+++ main.tf
@@ -1,1 +1,104 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+##############################
+#  Use Existing Resource Group
+##############################
+data "azurerm_resource_group" "existing_rg" {
+  name = "Machine_Terraform"
+}
+
+##############################
+# Virtual Network
+##############################
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+##############################
+# Subnet
+##############################
+resource "azurerm_subnet" "subnet" {
+  name                 = "terraform-subnet"
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+##############################
+# Public IP
+##############################
+resource "azurerm_public_ip" "publicip" {
+  name                = "vm-public-ip"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  allocation_method   = "Dynamic"
+}
+
+##############################
+# Network Interface
+##############################
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+##############################
+# Virtual Machine
+##############################
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "terraform-vm"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_password = "Admin@12345!"  # You can use SSH instead
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+##############################
+# Output Public IP
+##############################
+output "vm_public_ip" {
+  value = azurerm_public_ip.publicip.ip_address
+}
 
